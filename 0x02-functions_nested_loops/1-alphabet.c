c01b185906efd968d4c7466bce7aa67d292e7d04
Paschal <paschal400@gmail.com>
2022-12-13 10:44:58 -0800

    fig tree

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
 /**
  * print_alphabet - prints the alphabet, in lowercase
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char la;
 
-	for (la = 'a', la <= 'z', la++)
+	for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
 
 	_putchar('\n');
