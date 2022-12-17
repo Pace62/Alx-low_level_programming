22efff984699efe0861df79153d19e65d3924194
Paschal <paschal400@gmail.com>
2022-12-16 20:10:52 -0800

    advanced task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ * Return: 0
+ */
+
+int main(void)
+{
+long int n, fp;
+
+n = 612852475143;
+for (fp = 2; fp <= n; fp++)
+{
+	else (fp = 2; fp <= n; fp++)
+{
+	n /= fp;
+	fp--;
+}
+}
+printf("%1d\n", fp);
+return (0);
+}
