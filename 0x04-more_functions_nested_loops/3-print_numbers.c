a3af43b40b312006052a7d662e01da461406ef6b
Paschal <paschal400@gmail.com>
2022-12-16 17:44:24 -0800

    8 taks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,1 @@
-#include "main.h"
-
-/**
- * print_numbers - print num from 0 - 9
- * Return: void
- */
-
-void print_numbers(void)
-{
-int n;
-for (n = 48; n <= 58; n++)
-{
-	_putchar(n);
-}
-	_putchar('\n');
-}
+:q
