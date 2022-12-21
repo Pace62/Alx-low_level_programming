77727aa54f7b28d90b6db1ff01ac3b40e8b3b5f7
paschal <paschal400@gmail.com>
2022-12-21 07:20:32 -0800

    0 task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strcat - string to overwrite the (\0) at the end of dest and adds it again
+ * @dest: string
+ * @src: string
+ * Return: pointer to @dest.
+ */
+char *_strcat(char *dest, char *src);
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
